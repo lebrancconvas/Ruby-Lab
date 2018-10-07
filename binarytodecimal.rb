b24899850822f02fdcdd6aae86224d5e6a4640ce
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-10-08 01:18:51 +0700

    Code Edit. [Convert Binary Number to Decimal Number]

--- binarytodecimal.rb
+++ binarytodecimal.rb
@@ -1,10 +1,16 @@
+# Input -> Binary Number as Integer
+# Output -> Decimal Number as Integer
+
 def bitodec(bi)
-	turnbi = bi.chars.reverse.map{|i| i.to_i}
+	bistring = bi.to_s
+	turnbi = bistring.chars.reverse.map{|i| i.to_i}
 	for index in (0...turnbi.length)
 		turnbi[index] = turnbi[index]*(2**index)
 	end
-	return turnbi.sum
+	dec = turnbi.sum
+	return dec
 end
 
-puts bitodec("1011") # 11
-puts bitodec("1111") # 15
+puts bitodec(1011) # 11
+puts bitodec(1111) # 15
+puts bitodec(1100100100100) # 6436
